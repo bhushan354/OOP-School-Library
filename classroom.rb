a59432799abb7e1df7733dd3f1e9ef6e5eb26e8d
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-09 16:44:31 +0530

    Clean commit

--- classroom.rb
+++ classroom.rb
@@ -1,30 +1,29 @@
 class Classroom
-    attr_accessor :label
-    attr_reader :students
-  
-    def initialize(label)
-      @label = label
-      @students = []
-    end
-  
-    def add_student(student)
-      students << student
-      student.classroom = self
-    end
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
   end
-  
-  class Student
-    attr_accessor :name
-    attr_reader :classroom
-  
-    def initialize(name)
-      @name = name
-      @classroom = nil
-    end
-  
-    def classroom=(classroom)
-      @classroom = classroom
-      classroom.add_student(self)
-    end
+
+  def add_student(student)
+    students << student
+    student.classroom = self
   end
-  
+end
+
+class Student
+  attr_accessor :name
+  attr_reader :classroom
+
+  def initialize(name)
+    @name = name
+    @classroom = nil
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.add_student(self)
+  end
+end
