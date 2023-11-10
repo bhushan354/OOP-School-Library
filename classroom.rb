d7a1ad4a884578d88cc8c8378a19b5de8c6f479a
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-10 07:24:59 +0530

    Merged student class from classroom.rb to student.rb

--- classroom.rb
+++ classroom.rb
@@ -12,18 +12,3 @@ class Classroom
     student.classroom = self
   end
 end
-
-class Student
-  attr_accessor :name
-  attr_reader :classroom
-
-  def initialize(name)
-    @name = name
-    @classroom = nil
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.add_student(self)
-  end
-end
--- student.rb
+++ student.rb
@@ -1,10 +1,11 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom :name
 
   def initialize(classroom:, name: 'Unknown', age: 0, parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
+    @name = name
     @classroom = classroom
   end
 
