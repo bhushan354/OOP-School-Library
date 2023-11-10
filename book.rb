b051f1430cad0ca650b8825a15416deeb0321923
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-10 07:37:08 +0530

    Remove redundancy in code

--- book.rb
+++ book.rb
@@ -15,13 +15,3 @@ class Book
   end
 end
 
-class Rental
-  attr_accessor :date
-  attr_reader :book, :person
-
-  def initialize(book, person, date)
-    @book = book
-    @person = person
-    @date = date
-  end
-end
