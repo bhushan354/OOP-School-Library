5d26eb5505aeb8fb319d7a0d1d2e7bb6e4bde38c
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-10 21:08:06 +0530

    Configure rental

--- rental.rb
+++ rental.rb
@@ -1,16 +1,10 @@
 class Rental
-
-  attr_reader :book, :person
   attr_accessor :date
+  attr_reader :book, :person
 
   def initialize(book, person, date)
     @book = book
-    book.rentals <<self
-
     @person = person
-    person.rentals << self
-
     @date = date
   end
-
 end
