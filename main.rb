9031fb7357facc8184598f2a1d8f381eeef056f2
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-14 07:31:00 +0530

    add Execution related code to seperate files

--- main.rb
+++ main.rb
@@ -20,29 +20,6 @@ def all_options_show
   puts '7 - EXIT THE TERMINAL APP'
 end
 
-def execute_option(app, number)
-  option_methods = {
-    1 => :list_all_books,
-    2 => :list_persons,
-    3 => :create_person,
-    4 => :create_book_option,
-    5 => :create_rental,
-    6 => :get_rental_option,
-    7 => :exit_app
-  }
-
-  if option_methods.key?(number)
-    send(option_methods[number], app)
-  else
-    puts 'Enter valid number'
-    main(app)
-  end
-end
-
-def exit_app(*)
-  puts 'Thank you for using Terminal App, Hope you liked it !'
-end
-
 def create_person(app)
   print 'Do you want to create Student(1) or Teacher (2)? [Your Response]: '
   person_individual_id = gets.chomp.to_i
