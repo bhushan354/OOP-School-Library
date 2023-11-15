f896e70355c417982399e3c29a6e0412dc116709
bhushan354 <deshmukhbhushan354@gmail.com>
2023-11-16 04:54:53 +0530

    Fix errors for Rental class

--- class/rental.rb
+++ class/rental.rb
@@ -1,14 +1,16 @@
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
+  attr_accessor :date, :person, :book
 
-  def initialize(book, person, date)
-    @book = book
-    book.rentals << self
-
-    @person = person
-    person.rentals << self
-
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    @book = book
+
+    @person.rental << self
+    book.rental << self
+  end
+
+  def self.all
+    ObjectSpace.each_object(self).to_a
   end
 end
